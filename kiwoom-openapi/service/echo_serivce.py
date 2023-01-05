c8f4d5714fbca22e645b5a5becb4c57dac173d42
hdmun <huidae.mun@gmail.com>
2023-01-05 17:16:50 +0900

    feat: `EchoService` 에서 사용할 DTO 스펙 정리

--- kiwoom-openapi/service/echo_serivce.py
+++ kiwoom-openapi/service/echo_serivce.py
@@ -5,9 +5,10 @@ import asyncio
 
 import zmq
 import zmq.asyncio
+
+import protocol
 from openapi import ResponseError
 from openapi.client import KiwoomOpenAPIClient
-
 from router import Router
 
 
@@ -23,27 +24,27 @@ class EchoService(object):
         self._openapi_client = openapi_client
 
     @echo.route('/connect')
-    async def on_connect_openapi(self):
+    async def on_connect_openapi(self, _: protocol.RequestMessage):
         # try connect
         # timeout check
         # reply
         pass
 
     @echo.route('/get/stocks')
-    def on_get_stocks(self):
+    def on_get_stocks(self, request: protocol.GetStockRequest):
         # request openapi
         # reply
         pass
 
     @echo.route('/tick/market')
-    def on_get_tick_market(self):
+    def on_get_tick_market(self, request: protocol.TickMarketRequest):
         # request openapi
         # push
         # reply
         pass
 
     @echo.route('/investors')
-    def on_get_investors(self):
+    def on_get_investors(self, request: protocol.InvestorsRequest):
         # request openapi
         # push
         # reply
