b054374c3afc8d4a1c752686903ec8ba2872d7c2
hdmun <huidae.mun@gmail.com>
2023-01-12 00:54:03 +0900

    refactor: 호스트, 포트 설정 config 파일에서 불러오기
    파일명은 환경 변수에 설정해서 불러오자

--- kiwoom-openapi/main.py
+++ kiwoom-openapi/main.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 # -*-coding: utf-8 -*
 import asyncio
+import json
+import os
 
 import zmq.asyncio
 from PyQt5.QtWidgets import QApplication
+from model.config import Config
 
 from openapi.client import KiwoomOpenAPIClient
 from controller.echo_controller import EchoController
 from service.push_service import PushService
 
 
+def load_config(filename: str) -> Config:
+    with open(filename, 'r') as config_fp:
+        return Config(**json.load(fp=config_fp))
+
+
 async def main():
+    config = load_config(filename=os.environ['CONFIG_FILENAME'])
+
     zmqctx = zmq.asyncio.Context()
     openapi_client = KiwoomOpenAPIClient(qtapp=QApplication([]))
     push_service = PushService(zmqctx)
@@ -20,7 +30,7 @@ async def main():
         openapi_client=openapi_client
     )
 
-    echo_controller.bind(host='tcp://127.0.0.1', echo_port=7070, push_port=7071)
+    echo_controller.bind(config.host, config.echo_port, config.push_port)
     await asyncio.gather(
         echo_controller.listen(),
         push_service.start()
